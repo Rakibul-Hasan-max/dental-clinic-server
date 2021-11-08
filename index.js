3ce65f843a0e28bd46e8bf0a2b1054d12a7a2fe6
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-08 22:43:05 +0600

    add

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5r6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+async function run() {
+  try {
+    await client.connect();
+    // const database = client.db('sample_mflix');
+    // const movies = database.collection('movies');
+    // const query = { title: 'Back to the Future' };
+    // const movie = await movies.findOne(query);
+    // console.log(movie);
+    } 
+  finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Running Dental-Clinic server')
