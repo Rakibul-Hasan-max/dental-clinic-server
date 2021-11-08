58ce02c8cf6be89a35008ac18b7ed6533c2634ef
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-11-08 19:58:32 +0600

    new com

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5r6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
 
 app.get('/', (req, res) => {
   res.send('Running Dental-Clinic server')
